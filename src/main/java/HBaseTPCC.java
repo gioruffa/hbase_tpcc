3ef27215df339eab88690e04e4232a464e7d075e
Giorgio Ruffa <gioruffa@gmail.com>
2018-01-22 15:59:29 +0100

    loading completed

--- src/main/java/HBaseTPCC.java
+++ src/main/java/HBaseTPCC.java
@@ -85,6 +85,98 @@ public class HBaseTPCC {
                 new int [] {0}
         );
 
+        loadTableWrapper(
+                connection,
+                districtTableName,
+                districtColumnFamilyName,
+                folder,
+                "district.csv",
+                new String[] {
+                        "D_ID", "D_W_ID", "D_NAME", "D_STREET_1", "D_STREET_2", "D_CITY", "D_STATE", "D_ZIP", "D_TAX", "D_YTD", "D_NEXT_O_ID"
+                },
+                new int [] {1,0}
+        );
+
+        loadTableWrapper(
+                connection,
+                itemTableName,
+                itemColumnFamilyName,
+                folder,
+                "item.csv",
+                new String[] {
+                        "I_ID", "I_IM_ID", "I_NAME", "I_PRICE", "I_DATA"
+                },
+                new int [] {0}
+        );
+
+        loadTableWrapper(
+                connection,
+                new_orderTableName,
+                new_orderColumnFamilyName,
+                folder,
+                "new_order.csv",
+                new String[] {
+                        "NO_O_ID", "NO_D_ID", "NO_W_ID"
+                },
+                new int [] {2,1,0}
+        );
+
+        loadTableWrapper(
+                connection,
+                ordersTableName,
+                ordersColumnFamilyName,
+                folder,
+                "orders.csv",
+                new String[] {
+                        "O_ID", "O_D_ID", "O_W_ID", "O_C_ID", "O_ENTRY_D", "O_CARRIER_ID", "O_OL_CNT", "O_ALL_LOCAL"
+                },
+                new int [] {2,1,0}
+        );
+
+        //HISTORY -> TODO
+
+        loadTableWrapper(
+                connection,
+                customerTableName,
+                customerColumnFamilyName,
+                folder,
+                "customer.csv",
+                new String[] {
+                        "C_ID", "C_D_ID", "C_W_ID", "C_FIRST", "C_MIDDLE", "C_LAST", "C_STREET_1",
+                        "C_STREET_2", "C_CITY", "C_STATE", "C_ZIP", "C_PHONE", "C_SINCE", "C_CREDIT", "C_CREDITLIM",
+                        "C_DISCOUNT", "C_BALANCE", "C_YTD_PAYMENT", "C_PAYMENT_CNT", "C_DELIVERY_CNT", "C_DATA"
+                },
+                new int [] {2,1,0}
+        );
+
+        loadTableWrapper(
+                connection,
+                stockTableName,
+                stockColumnFamilyName,
+                folder,
+                "stock.csv",
+                new String[] {
+                        "S_I_ID", "S_W_ID", "S_QUANTITY", "S_DIST_01", "S_DIST_02", "S_DIST_03", "S_DIST_04",
+                        "S_DIST_05", "S_DIST_06", "S_DIST_07", "S_DIST_08", "S_DIST_09", "S_DIST_10", "S_YTD",
+                        "S_ORDER_CNT", "S_REMOTE_CNT", "S_DATA"
+                },
+                new int [] {1,0}
+        );
+
+        loadTableWrapper(
+                connection,
+                order_lineTableName,
+                order_lineColumnFamilyName,
+                folder,
+                "order_line.csv",
+                new String[] {
+                        "OL_O_ID", "O_D_ID", "OL_W_ID", "OL_NUMBER", "OL_I_ID", "OL_SUPPLY_W_ID",
+                        "OL_DELIVERY_D", "OL_QUANTITY", "OL_AMOUNT", "OL_DIST_INFO"
+                },
+                new int [] {2,1,0,3}
+        );
+
+
 
 
         //TO IMPLEMENT
